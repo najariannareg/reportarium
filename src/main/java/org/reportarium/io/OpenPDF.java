47899c3a64e5c9c2c4e9058d647d568be5e97644
nnajarian <nnajarian@vmware.com>
2025-05-11 21:51:47 +0400

    Formatting

--- src/main/java/org/reportarium/io/OpenPDF.java
+++ src/main/java/org/reportarium/io/OpenPDF.java
@@ -1,7 +1,10 @@
 package org.reportarium.io;
 
 import com.lowagie.text.Document;
+import com.lowagie.text.Element;
 import com.lowagie.text.Font;
+import com.lowagie.text.ListItem;
+import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.PdfWriter;
@@ -20,7 +23,7 @@ public class OpenPDF {
 
     public void write(String filename, Map<String, Map<String, String>> items) {
         try {
-            Document document = new Document();
+            Document document = new Document(PageSize.A4, 50, 50, 50, 50);
             PdfWriter.getInstance(document, new FileOutputStream(filename));
             document.open();
 
@@ -32,13 +35,28 @@ public class OpenPDF {
                 BaseFont baseFont = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED, false, inputStream.readAllBytes(), null);
                 Font font = new Font(baseFont, 12, Font.NORMAL);
 
-                document.add(new Paragraph("Violations Report"));
+                Paragraph title = new Paragraph("ՀԱՇՎԵՏՎՈՒԹՅՈՒՆ ԽԱԽՏՈՒՄՆԵՐԻ ՄԱՍԻՆ", new Font(baseFont, 16, Font.BOLD));
+                title.setAlignment(Element.ALIGN_CENTER);
+                title.setSpacingAfter(20f);
+                document.add(title);
+
+                Paragraph heading = new Paragraph("ՀԱՎԵԼՎԱԾ 5:", new Font(baseFont, 14, Font.UNDERLINE));
+                heading.setAlignment(Element.ALIGN_LEFT);
+                heading.setSpacingAfter(20f);
+                document.add(heading);
+
+                com.lowagie.text.List pdfList = new com.lowagie.text.List(true, 20f);
 
                 for (Map.Entry<String, Map<String, String>> item : items.entrySet()) {
                     Map<String, String> values = item.getValue();
                     String text = item.getKey() + ": " + values.get(DESCRIPTION) + "\n" + values.get(JUSTIFICATION);
-                    document.add(new Paragraph(text, font));
+
+                    ListItem listItem = new ListItem(text, font);
+                    listItem.setSpacingAfter(10f);
+                    pdfList.add(listItem);
                 }
+
+                document.add(pdfList);
             } catch (IOException e) {
                 System.err.println("Error accessing font: " + e.getMessage());
                 return;

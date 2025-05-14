5d0e3a4b5300f9ac1b01330cf4449c7eddc9caf4
nnajarian <nnajarian@vmware.com>
2025-05-14 13:51:14 +0400

    Fixed typo and Form order.

--- src/main/java/org/reportarium/generator/ReportGenerator.java
+++ src/main/java/org/reportarium/generator/ReportGenerator.java
@@ -5,17 +5,23 @@ import org.reportarium.io.OpenCSV;
 import org.reportarium.io.OpenPDF;
 import org.reportarium.model.Item;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class ReportGenerator {
 
-    public static final Map<String, String> FORMS = Map.of(
-            "Հավելված 5", "Form-5.csv",
-            "Հավելված 6", "Form-6.csv",
-            "Հավելված 6.1", "Form-6.1.csv"
-    );
+    public static final Map<String, String> FORMS = createFormsMap();
+
+    private static Map<String, String> createFormsMap() {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("Հավելված 5", "Form-5.csv");
+        map.put("Հավելված 6", "Form-6.csv");
+        map.put("Հավելված 6.1", "Form-6.1.csv");
+        return Collections.unmodifiableMap(map);
+    }
 
     public String generate() {
         DialogReader dialog = new DialogReader();
@@ -30,9 +36,9 @@ public class ReportGenerator {
         return "Պարոն Անդրանիկ Հովակիմյան ջան, \n" +
                 "ձեր հաշվետվությունը պատրաստ է։ \n\n" +
                 "Այն կարող եք գտնել հետևյալ հասցեով՝ \n\n" +
-                path + "\n\n" +
+                path + " \n\n" +
                 "Սիրով, \n" +
-                "Նարեկ նաճարեանի և Ալինա Հովակիմյանի կողմից";
+                "Նարեկ Նաճարեանի և Ալինա Հովակիմյանի կողմից ";
     }
 
 }
